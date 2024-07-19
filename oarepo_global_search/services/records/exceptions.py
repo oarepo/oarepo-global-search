bd35eec80e9bd341e303fc996469d020f6ce46af
Alžběta Pokorná <68014925+Alzpeta@users.noreply.github.com>
2024-07-19 10:49:30 +0200

    Update exceptions.py

--- oarepo_global_search/services/records/exceptions.py
+++ oarepo_global_search/services/records/exceptions.py
@@ -3,11 +3,3 @@ class InvalidServicesError(Exception):
         self.message = message
         super().__init__(self.message)
 
-
-class PermissionDeniedError(Exception):
-    def __init__(
-        self,
-        message="You do not have the required permissions to perform this operation.",
-    ):
-        self.message = message
-        super().__init__(self.message)
